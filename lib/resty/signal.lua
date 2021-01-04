fd2f35dfc53d9b699acae7bb786caa5f5fe792a0
zhaoxiaobiao <zhaoxiaobiaogm@gmail.com>
2021-01-04 18:48:08 +0800

    mv signum to local function

--- lib/resty/signal.lua
+++ lib/resty/signal.lua
@@ -112,15 +112,37 @@ local signals = {
 }
 
 
+local function signum(name)
+    local sig_num
+    if type(name) == "number" then
+        sig_num = name
+    else
+        local id = signals[name]
+        if not id then
+            return nil, "unknown signal name"
+        end
+
+        sig_num = tonumber(resty_signal.resty_signal_signum(id))
+        if sig_num < 0 then
+            error(
+                string_format("missing C def for signal %s = %d", name, id),
+                2
+            )
+        end
+    end
+    return sig_num
+end
+
+
 function _M.kill(pid, sig)
     assert(sig)
 
-    local signum, err = _M.signum(sig)
+    local sig_num, err = signum(sig)
     if err then
         return nil, err
     end
 
-    local rc = tonumber(C.kill(assert(pid), signum))
+    local rc = tonumber(C.kill(assert(pid), sig_num))
     if rc == 0 then
         return true
     end
@@ -129,26 +151,6 @@ function _M.kill(pid, sig)
     return nil, err
 end
 
-
-function _M.signum(name)
-    local signum
-    if type(name) == "number" then
-        signum = name
-    else
-        local id = signals[name]
-        if not id then
-            return nil, "unknown signal name"
-        end
-
-        signum = tonumber(resty_signal.resty_signal_signum(id))
-        if signum < 0 then
-            error(
-                string_format("missing C def for signal %s = %d", name, id),
-                2
-            )
-        end
-    end
-    return signum
-end
+_M.signum = signum
 
 return _M
